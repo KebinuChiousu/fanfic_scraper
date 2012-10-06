ba3cc5888086c9bf70616cf2f44d70229cae8e5c
Kebinu Chiousu <kebinu.chiousu@gmail.com>
2012-10-05 21:39:15 -0700

    Began Abstraction of Data Access Layer (DAL)

---
+++ Data/DAL/clsDAL.vb
@@ -0,0 +1,15 @@
+﻿'
+' Created by SharpDevelop.
+' User: Kevin
+' Date: 10/5/2012
+' Time: 9:31 PM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public MustInherit Class DAL
+	
+	Public MustOverride Function GetConnectionString(ConnStr)
+	Public MustOverride Function SetConnectionString(ConnStr)
+	
+	
+End Class
