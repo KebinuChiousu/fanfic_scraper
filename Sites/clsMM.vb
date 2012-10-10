b8c7196f94057202f77cf7a1c82fa71ea1881f9f
Kebinu Chiousu <kebinu.chiousu@gmail.com>
2012-10-09 22:47:33 -0700

    Support for MediaMiner (MM) Completed!

--- Sites/clsMM.vb
+++ Sites/clsMM.vb
@@ -11,43 +11,7 @@ Public Class MM
 
     Private Browser As clsWeb
 
-    Public Overrides Function GetAuthorURL(ByVal link As String) As String
-
-    End Function
-
-    Public Overrides Function GetStoryID(ByVal link As String) As String
-
-        Dim ret As String = ""
-        Dim url As URL
-
-        url = ExtractUrl(link)
-        ret = url.Query(0).Value
-
-        Return ret
-
-    End Function
-
-    Public Overrides Function GetStoryURL(ByVal id As String) As String
-
-        Dim link As String
-
-        link = "http://mediaminer.org/fanfic/view_st.php?id=" & id
-
-        Return link
-
-    End Function
-
-    Public Overrides Function GrabAuthor(ByVal htmlDoc As String) As String
-
-    End Function
-
-    Public Overrides Function GrabBody(ByVal htmldoc As String) As String
-
-    End Function
-
-    Public Overrides Function GrabDate(ByVal htmlDoc As String, ByVal title As String) As String
-
-    End Function
+#Region "Downloading HTML"
 
     Public Overrides Function GrabData(ByVal url As String) As String
 
@@ -58,6 +22,10 @@ Public Class MM
         Return htmldoc
 
     End Function
+
+#End Region
+
+#Region "RSS"
 
     Public Overrides Function GrabFeed(ByRef rss As String) As System.Xml.XmlDocument
 
@@ -71,11 +39,9 @@ Public Class MM
 
         Dim title As String
         Dim link As String
-        Dim url As URL
 
         Dim node_idx As Integer
 
-        Dim author As String
         Dim author_url As String
 
         Dim summary() As String
@@ -154,7 +120,7 @@ Public Class MM
             End If
 
             fic(node_idx).UpdateDate = CDate(dummy).ToShortDateString
-           
+
             doc = CleanHTML(summary(6))
             dummy = doc.DocumentNode.InnerText
             dummy = HtmlDecode(dummy)
@@ -165,12 +131,15 @@ Public Class MM
 
             doc = CleanHTML(html)
             temp = FindNodesByAttribute(doc.DocumentNode, "select", "name", "cid", False)
-            temp = doc.DocumentNode.SelectNodes("//option")
 
             If IsNothing(temp) Then
                 fic(node_idx).ChapterCount = 1
                 fic(node_idx).PublishDate = fic(node_idx).UpdateDate
             Else
+
+                dummy = temp(0).InnerHtml
+                doc = CleanHTML(dummy)
+                temp = doc.DocumentNode.SelectNodes("//option")
 
                 fic(node_idx).ChapterCount = temp.Count
 
@@ -215,10 +184,6 @@ Public Class MM
 
     End Function
 
-    Public Overrides Function GrabSeries(ByVal htmlDoc As String) As String
-
-    End Function
-
     Public Overrides Function GrabStoryInfo(ByRef dsRSS As System.Data.DataSet, ByVal idx As Integer) As clsFanfic.Story
 
         Dim fic As New clsFanfic.Story
@@ -251,30 +216,337 @@ Public Class MM
 
         Return fic
 
+    End Function
+
+#End Region
+
+#Region "Chapter Navigation"
+
+    Public Overrides Function GetChapters(htmlDoc As String) As String()
+
+        Dim ret() As String
+
+        Dim doc As HtmlDocument
+        Dim temp As HtmlNodeCollection
+
+        Dim idx As Integer
+
+        'Clean up html into xhtml
+        doc = CleanHTML(htmlDoc)
+
+        temp = FindNodesByAttribute(doc.DocumentNode, "select", "name", "cid", False)
+
+        If Not IsNothing(temp) Then
+
+            htmlDoc = temp(0).InnerHtml
+            doc = CleanHTML(htmlDoc)
+            temp = doc.DocumentNode.SelectNodes("//option")
+
+            ReDim ret(temp.Count - 1)
+            For idx = 0 To temp.Count - 1
+                ret(idx) = temp(idx).Attributes("value").Value
+            Next
+        Else
+            ReDim ret(0)
+            ret(0) = "Chapter 1"
+        End If
+
+        Me.Chapters = ret
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function ProcessChapters(ByVal link As String, ByVal index As Integer) As String
+
+        Dim htmldoc As String
+
+        If UBound(Me.Chapters) > 1 Then
+            link = GetChapterURL(link, Me.Chapters(index))
+        End If
+
+        htmldoc = GrabData(link)
+
+        Return htmldoc
+
+    End Function
+
+    Private Function GetChapterURL(link As String, index As Integer) As String
+
+        Dim ret As String
+
+        ret = "http://mediaminer.org/fanfic/view_ch.php?cid="
+        ret += CStr(index)
+        ret += "&"
+        ret += "id="
+        ret += GetStoryID(link)
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function GetAuthorURL(ByVal link As String) As String
+
+        Dim ret As String
+
+        Dim temp() As String
+
+        If InStr(link, "/src.php/u") > 0 Then
+
+            temp = Split(link, "/")
+
+            link += "http://mediaminer.org/user_info.php/" & temp(UBound(temp))
+
+            ret = link
+        Else
+            If InStr(link, "user_info.php/") > 0 Then
+                ret = link
+            Else
+                Return ""
+            End If
+        End If
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function GetStoryID(ByVal link As String) As String
+
+        Dim ret As String = ""
+        Dim url As URL
+
+        url = ExtractUrl(link)
+        ret = url.Query(0).Value
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function GetStoryURL(ByVal id As String) As String
+
+        Dim link As String
+
+        link = "http://mediaminer.org/fanfic/view_st.php?id=" & id
+
+        Return link
+
+    End Function
+
+#End Region
+
+#Region "HTML Processing"
+
+    Private Function GrabHeaderInfo(ByVal htmldoc As String) As String()
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        Dim dummy As String
+        Dim summary() As String
+
+        doc = CleanHTML(htmldoc)
+
+        temp = doc.DocumentNode.SelectNodes("//table")
+        doc = CleanHTML(temp(4).InnerHtml)
+        temp = doc.DocumentNode.SelectNodes("//td")
+        dummy = temp(1).InnerHtml
+
+        summary = Split(dummy, "<br />")
+
+        doc = Nothing
+        temp = Nothing
+
+        Return summary
+
+    End Function
+
+    Public Overrides Function GrabAuthor(ByVal htmlDoc As String) As String
+
+        Dim author As String
+
+        Dim idx As Integer
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        Dim dummy As String
+        Dim summary() As String
+
+        summary = GrabHeaderInfo(htmlDoc)
+
+        idx = 2
+        dummy = summary(idx)
+        doc = CleanHTML(dummy)
+        temp = doc.DocumentNode.SelectNodes("//a")
+
+        If IsNothing(temp) Then
+            idx = 3
+            doc = CleanHTML(summary(idx))
+            temp = doc.DocumentNode.SelectNodes("//a")
+        End If
+
+        author = temp(0).InnerText
+
+        doc = Nothing
+        temp = Nothing
+
+        Return author
+
+    End Function
+
+    Public Overrides Function GrabBody(ByVal htmldoc As String) As String
+
+        Dim idx As Integer
+
+        Dim ret As String
+
+        Dim content As String
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        doc = CleanHTML(htmldoc)
+
+        '<div align="left" style=" padding: 0.00mm 0.00mm 0.00mm 0.00mm;">
+
+        temp = FindNodesByAttribute(doc.DocumentNode, "div", "style", "padding: 0.00mm 0.00mm 0.00mm 0.00mm;", True)
+
+        ret = "<html>"
+        ret += "<body>"
+
+        For idx = 0 To temp.Count - 1
+
+            content = HtmlDecode(HtmlDecode(temp(idx).OuterHtml))
+            content = Replace(content, "<br />", "<br /><br />")
+
+            ret += content
+            ret += "<br /><br />"
+
+
+        Next
+
+        ret += "</body>"
+        ret += "</html>"
+
+        doc = Nothing
+        temp = Nothing
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function GrabDate(ByVal htmlDoc As String, ByVal title As String) As String
+
+        Dim ret As String
+
+        Dim idx As Integer
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        Dim dummy As String
+        Dim summary() As String
+
+        summary = GrabHeaderInfo(htmlDoc)
+
+        idx = 2
+        dummy = summary(idx)
+        doc = CleanHTML(dummy)
+        temp = doc.DocumentNode.SelectNodes("//a")
+
+        If IsNothing(temp) Then
+            idx = 3
+        End If
+
+        If idx = 3 Then
+            idx = 2
+        Else
+            idx = 3
+        End If
+
+        dummy = Split(summary(idx), "</b>")(1)
+        dummy = Trim(Split(dummy, "|")(0))
+        If (InStr(dummy, "DT") = Len(dummy) - 1) Or (InStr(dummy, "ST") = Len(dummy) - 1) Then
+            dummy = Mid(dummy, 1, Len(dummy) - 3)
+        End If
+
+        ret = CDate(dummy).ToShortDateString
+
+        doc = Nothing
+        temp = Nothing
+
+        Return ret
+
+    End Function
+
+    Public Overrides Function GrabSeries(ByVal htmlDoc As String) As String
+
+        Dim series As String
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        Dim summary() As String
+
+        summary = GrabHeaderInfo(htmlDoc)
+
+        doc = CleanHTML(summary(0))
+        temp = doc.DocumentNode.SelectNodes("//a")
+
+        series = temp(0).InnerText
+
+        doc = Nothing
+        temp = Nothing
+
+        Return series
 
     End Function
 
     Public Overrides Function GrabTitle(ByVal htmlDoc As String) As String
 
+        Dim ret As String
+        Dim pos As Integer
+
+        Dim doc As HtmlDocument = Nothing
+        Dim temp As HtmlNodeCollection
+
+        doc = CleanHTML(htmlDoc)
+        temp = FindNodesByAttribute(doc.DocumentNode, "td", "class", "ffh", False)
+
+        ret = temp(0).InnerText
+
+        pos = InStr(ret, ":")
+
+        If pos > 1 Then
+            ret = Mid(ret, 1, pos - 1)
+        End If
+
+        doc = Nothing
+        temp = Nothing
+
+        Return ret
+
     End Function
 
-    
-
-    Public Overrides Function ProcessChapters(ByVal link As String, ByVal index As Integer) As String
-
-    End Function
+#End Region
 
 #Region "Properties"
 
     Public Overrides ReadOnly Property ErrorMessage() As String
         Get
 
+            Dim ret As String
+
+            ret = "<h2 style=""margin: 5px;"">Jump to Anime Series:</h2>"
+
+            Return ret
+
         End Get
     End Property
 
     Public Overrides ReadOnly Property HostName() As String
         Get
-        	Return "mediaminer.org"        	
+            Return "mediaminer.org"
         End Get
     End Property
 
