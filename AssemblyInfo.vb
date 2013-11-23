9860da9364f1722a1248a526d0756fc4693c4b8e
Kevin Meredith <kebinu.chiousu@gmail.com>
2013-11-23 14:29:41 -0800

    Version Changed to 0.5.5

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -29,4 +29,4 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("0.5.4.*")> 
+<Assembly: AssemblyVersion("0.5.5.*")> 
