3ec2f149c5ebcfc51fbd1ca051776bcb7e163c76
Kevin Meredith <kebinu.chiousu@gmail.com>
2014-08-16 15:25:00 -0700

    HPFFA - RSS Functional

--- BusinessLogic/Sites/clsHPFFA.vb
+++ BusinessLogic/Sites/clsHPFFA.vb
@@ -47,9 +47,19 @@ Public Class HPFFA
 
     Public Overrides Function GetStoryID(link As String) As String
 
+        Dim ret As String = ""
+        Dim url As URL
+
+        url = ExtractUrl(link)
+        ret = url.Query(0).Value
+
+        Return ret
+
     End Function
 
     Public Overrides Function GetStoryURL(id As String) As String
+
+        Return "http://" & Me.HostName & "/stories/viewstory.php?sid=" & id
 
     End Function
 
@@ -62,20 +72,22 @@ Public Class HPFFA
 
         Dim html As String
         Dim doc As HtmlDocument = Nothing
+        Dim tdoc As HtmlDocument = Nothing
 
         Dim nodes As HtmlNodeCollection
         Dim temp As HtmlNodeCollection
 
         Dim dummy As String
 
-        Dim title As String
-        Dim link As String
-
+        Dim temp_idx As Integer
         Dim node_idx As Integer
 
         Dim author_url As String
 
         Dim summary() As String
+
+        Dim ch_idx As Integer
+
         Dim fic() As clsFanfic.Story
 
         Dim idx As Integer
@@ -94,104 +106,90 @@ Public Class HPFFA
 
         For node_idx = 0 To nodes.Count - 1
 
-            fic(node_idx).ID = nodes(node_idx).Attributes("value").Value
-            fic(node_idx).StoryURL = "http://mediaminer.org/fanfic/view_st.php?id=" & fic(node_idx).ID
+            doc = CleanHTML(nodes(node_idx).InnerHtml)
 
-            title = nodes(node_idx).NextSibling.InnerText
-            If InStr(title, "(") > 0 Then
-                fic(node_idx).Title = Mid(title, 1, InStr(title, "(") - 1)
-            Else
-                fic(node_idx).Title = title
-            End If
+            temp = FindNodesByAttribute(doc.DocumentNode, "div", "class", "title", False)
+            tdoc = CleanHTML(temp(0).InnerHtml)
+            temp = tdoc.DocumentNode.SelectNodes("//a")
 
-            html = Me.GrabData(fic(node_idx).StoryURL)
+            fic(node_idx).Category = "Harry Potter"
+
+            fic(node_idx).StoryURL = "http://" & Me.HostName & "/stories/" & temp(0).Attributes("href").Value
+            fic(node_idx).AuthorURL = "http://" & Me.HostName & "/stories/" & temp(1).Attributes("href").Value
+
+            fic(node_idx).Title = temp(0).InnerText
+            fic(node_idx).Author = temp(1).InnerText
+
+
+            temp = FindNodesByAttribute(doc.DocumentNode, "div", "class", "tail", False)
+
+            dummy = temp(0).InnerHtml
+
+            tdoc = CleanHTML(dummy)
+
+            temp = FindNodesByAttribute(doc.DocumentNode, "span", "class", "label", False)
+
+            For temp_idx = 0 To temp.Count - 1
+
+                dummy = Replace(dummy, temp(temp_idx).OuterHtml, "|" & temp(temp_idx).InnerText)
+
+            Next
+
+            dummy = Replace(dummy, "</span>", "")
+            summary = Split(dummy, "|")
+
+            fic(node_idx).PublishDate = CDate(Mid(summary(1), InStr(summary(1), ":") + 1)).ToShortDateString
+            fic(node_idx).UpdateDate = CDate(Mid(summary(2), InStr(summary(2), ":") + 1)).ToShortDateString
+
+            temp = FindNodesByAttribute(doc.DocumentNode, "div", "class", "content", False)
+
+            html = temp(0).InnerHtml
+
             doc = CleanHTML(html)
 
-            temp = doc.DocumentNode.SelectNodes("//table")
-            doc = CleanHTML(temp(4).InnerHtml)
-            temp = doc.DocumentNode.SelectNodes("//td")
-            dummy = temp(1).InnerHtml
+            temp = FindNodesByAttribute(doc.DocumentNode, "span", "class", "label", False)
 
-            summary = Split(dummy, "<br />")
+            For temp_idx = 0 To temp.Count - 1
 
-            doc = CleanHTML(summary(0))
-            temp = doc.DocumentNode.SelectNodes("//a")
+                html = Replace(html, temp(temp_idx).OuterHtml, "|" & temp(temp_idx).InnerText)
 
-            fic(node_idx).Category = temp(0).InnerText
+            Next
 
-            idx = 2
-            dummy = summary(idx)
-            doc = CleanHTML(dummy)
-            temp = doc.DocumentNode.SelectNodes("//a")
+            summary = Split(html, "|")
 
-            If IsNothing(temp) Then
-                idx = 3
-                doc = CleanHTML(summary(idx))
-                temp = doc.DocumentNode.SelectNodes("//a")
-            End If
+            tdoc = CleanHTML(summary(1))
 
-            fic(node_idx).Author = temp(0).InnerText
-            fic(node_idx).AuthorURL = "http://" & Me.HostName & temp(0).Attributes("href").Value
+            dummy = tdoc.DocumentNode.InnerText & vbCrLf
 
-            If idx = 3 Then
-                idx = 2
-            Else
-                idx = 3
-            End If
+            For idx = 0 To UBound(summary)
+                If InStr(summary(idx), "Chapters:") <> 0 Then
+                    ch_idx = idx
+                    Exit For
+                End If
+            Next
 
-            dummy = Split(summary(idx), "</b>")(1)
-            dummy = Trim(Split(dummy, "|")(0))
-            If (InStr(dummy, "DT") = Len(dummy) - 1) Or (InStr(dummy, "ST") = Len(dummy) - 1) Then
-                dummy = Mid(dummy, 1, Len(dummy) - 3)
-            End If
+            For idx = 2 To (ch_idx - 2)
 
-            fic(node_idx).UpdateDate = CDate(dummy).ToShortDateString
+                If idx <> 4 Then
 
-            doc = CleanHTML(summary(6))
-            dummy = doc.DocumentNode.InnerText
-            dummy = HtmlDecode(dummy)
-            dummy = HtmlDecode(dummy)
-            dummy = Replace(dummy, Chr(160), "")
+                    tdoc = CleanHTML(summary(idx))
+                    dummy += HtmlDecode(tdoc.DocumentNode.InnerText) & vbCrLf
+
+                End If
+
+            Next
 
             fic(node_idx).Summary = dummy
 
-            doc = CleanHTML(html)
-            temp = FindNodesByAttribute(doc.DocumentNode, "select", "name", "cid", False)
+            tdoc = CleanHTML(summary(ch_idx))
 
-            If IsNothing(temp) Then
-                fic(node_idx).ChapterCount = 1
-                fic(node_idx).PublishDate = fic(node_idx).UpdateDate
-            Else
+            dummy = tdoc.DocumentNode.InnerText
+            dummy = Left(dummy, InStr(dummy, "Table") - 1)
+            dummy = Mid(dummy, InStr(dummy, ":") + 1)
 
-                dummy = temp(0).InnerHtml
-                doc = CleanHTML(dummy)
-                temp = doc.DocumentNode.SelectNodes("//option")
+            fic(node_idx).ChapterCount = Trim(dummy)
 
-                fic(node_idx).ChapterCount = temp.Count
-
-                link = "http://mediaminer.org/fanfic/view_ch.php?cid="
-                link += temp(0).Attributes("value").Value
-                link += "&id=" & fic(node_idx).ID
-
-                html = Me.GrabData(link)
-                doc = CleanHTML(html)
-
-                temp = doc.DocumentNode.SelectNodes("//table")
-                doc = CleanHTML(temp(4).InnerHtml)
-                temp = doc.DocumentNode.SelectNodes("//td")
-                html = temp(1).InnerHtml
-
-                summary = Split(html, "<br />")
-
-                dummy = Split(summary(3), "</b>")(1)
-                dummy = Trim(Split(dummy, "|")(0))
-                If (InStr(dummy, "DT") = Len(dummy) - 1) Or (InStr(dummy, "ST") = Len(dummy) - 1) Then
-                    dummy = Mid(dummy, 1, Len(dummy) - 3)
-                End If
-
-                fic(node_idx).PublishDate = CDate(dummy).ToShortDateString
-
-            End If
+            fic(node_idx).ID = GetStoryID(fic(node_idx).StoryURL)
 
         Next node_idx
 
@@ -216,6 +214,38 @@ Public Class HPFFA
 
     Public Overrides Function GrabStoryInfo(idx As Integer) As clsFanfic.Story
 
+        Dim dsRSS As DataSet = MyBase.datasetRSS
+
+        Dim fic As New clsFanfic.Story
+
+        Dim temp() As String
+
+        ' Story Name
+        fic.Title = dsRSS.Tables("entry"). _
+                        Rows(idx).Item("title")
+
+        ' Story Author
+        fic.Author = dsRSS.Tables("author"). _
+                          Rows(idx).Item(0)
+        ' Story Location
+        fic.StoryURL = dsRSS.Tables("link"). _
+                          Rows(idx).Item(1)
+
+        fic.ID = Me.GetStoryID(fic.StoryURL)
+
+        fic.PublishDate = CDate(dsRSS.Tables("entry").Rows(idx).Item("published"))
+
+        fic.UpdateDate = CDate(dsRSS.Tables("entry").Rows(idx).Item("updated"))
+
+        temp = Split(dsRSS.Tables("entry").Rows(idx).Item("id"), ":")
+
+        fic.Category = temp(1)
+        fic.ChapterCount = temp(2)
+
+        fic.Summary = dsRSS.Tables("summary").Rows(idx).Item("summary_Text")
+
+        Return fic
+
     End Function
 
 #End Region
@@ -224,7 +254,7 @@ Public Class HPFFA
 
     Public Overrides ReadOnly Property HostName() As String
         Get
-            Return "hpfanficarchive.com"
+            Return "www.hpfanficarchive.com"
         End Get
     End Property
 
