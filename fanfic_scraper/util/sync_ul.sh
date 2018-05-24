167793a8c13cc05fa1678d3f5263c38e53b538ca
Kevin Meredith <kevin@meredithkm.info>
2018-05-24 18:30:20 -0400

    Update sync script to remove empty directories in Target.

--- fanfic_scraper/util/sync_ul.sh
+++ fanfic_scraper/util/sync_ul.sh
@@ -1,4 +1,4 @@
 rclone sync -v /opt/home/ubuntu/OneDrive/FF_Archive onedrive:FF_Archive 
+rclone rmdirs --leave-root -v onedrive:FF_Archive
 rclone sync -v /opt/home/ubuntu/OneDrive/FF_Archive/Current_FF onedrive:Fanfiction
-#rclone sync -v /opt/home/ubuntu/OneDrive/Fiction onedrive:Fiction
- 
+rclone rmdirs --leave-root -v onedrive:Fanfiction 
