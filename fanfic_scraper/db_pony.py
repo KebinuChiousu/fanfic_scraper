646eca303c3456e277c2c5c7bb7c39aea119f2db
Kevin Meredith <kevin@meredithkm.info>
2018-12-16 11:53:31 -0500

    Refactor db code.

--- fanfic_scraper/db_pony.py
+++ fanfic_scraper/db_pony.py
@@ -1,15 +1,24 @@
+import sys
 import datetime
-from pony.orm import *
+from pony.orm import Database, DatabaseError, Required, PrimaryKey, Optional
+from pony.orm import sql_debug
 
 db = Database()
+
 
 def init_db(filename):
     bind_db(filename)
     db.generate_mapping(create_tables=True)
 
+
 def bind_db(filename):
 
-    db.bind(provider='sqlite',filename=filename,create_db=True)
+    try:
+        db.bind(provider='sqlite', filename=filename, create_db=True)
+    except DatabaseError as err:
+        print("Error loading database!")
+        sys.exit(1)
+
 
 #############################################################
 class Category(db.Entity):
@@ -17,7 +26,8 @@ class Category(db.Entity):
     Pony ORM model of the Category table
     """
     Name = Required(str)
-    Id = PrimaryKey(int,auto=True)
+    Id = PrimaryKey(int, auto=True)
+
 
 #############################################################
 class Fanfic(db.Entity):
@@ -34,19 +44,21 @@ class Fanfic(db.Entity):
     Description = Optional(str)
     Internet = Optional(str)
     StoryId = Optional(str)
-    Abandoned = Optional(bool,default=0)
-    Complete = Optional(bool,default=0)
+    Abandoned = Optional(bool, default=0)
+    Complete = Optional(bool, default=0)
     Publish_Date = Optional(datetime.date)
     Update_Date = Optional(datetime.date)
     Last_Checked = Optional(datetime.date)
-    Id = PrimaryKey(int,auto=True)
+    Id = PrimaryKey(int, auto=True)
     Category_Id = Required(int)
+
 
 class DataBaseLogic:
 
     def __init__(self, db_filename):
 
         init_db(db_filename)
+
 
 def main():
 
@@ -58,6 +70,7 @@ def main():
     # map the models to the database
     # and create the tables, if they don't exist
     db.generate_mapping(create_tables=True)
+
 
 if __name__ == '__main__':
     main()
