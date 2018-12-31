655f0581392d0504fd813105ad3319146ba21d0c
Kevin Meredith <kevin@meredithkm.info>
2018-12-30 22:12:29 -0500

    Fix linting errors in ffdl.py

--- fanfic_scraper/ffdl.py
+++ fanfic_scraper/ffdl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+
 import os
 import sys
-import shutil
 import fnmatch
 import subprocess
 import argparse
@@ -9,14 +9,10 @@ from datetime import date
 from collections import defaultdict
 from configparser import SafeConfigParser
 from fanfic_scraper import current_fanfic
-import tldextract
-from urllib.parse import urlparse
-import requests
-from pony.orm import *
+from pony.orm import db_session, select, count
 from fanfic_scraper.db_pony import DataBaseLogic, Category, Fanfic
 from fanfic_scraper import cui
 import textwrap
-import sqlite3
 from time import sleep
 
 
@@ -27,17 +23,20 @@ db_name = 'FanfictionDB.db'
 db_folder = 'Read'
 dl_folder = 'htm'
 
+
 def ensure_dir(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+
 def get_config():
-    #create config dir if it doesn't exist and return path to config.
+    # create config dir if it doesn't exist and return path to config.
     home = os.path.expanduser("~")
-    cfg = os.path.join(home,".ff")
+    cfg = os.path.join(home, ".ff")
     ensure_dir(cfg)
 
-    return os.path.join(cfg,"config.ini")
+    return os.path.join(cfg, "config.ini")
+
 
 def config_load():
     global basePath
@@ -58,13 +57,13 @@ def config_load():
         config.read(cfg)
 
         try:
-            t_path = config.get('path','path')
-            t_root = config.get('path','rootfolder')
-            t_dbfolder = config.get('path','dbfolder')
-            t_dbname = config.get('path','dbname')
-            t_dlfolder = config.get('path','downloadfolder')
+            t_path = config.get('path', 'path')
+            t_root = config.get('path', 'rootfolder')
+            t_dbfolder = config.get('path', 'dbfolder')
+            t_dbname = config.get('path', 'dbname')
+            t_dlfolder = config.get('path', 'downloadfolder')
         except:
-            i=0
+            i = 0
 
         if t_path:
             basePath = t_path
@@ -77,6 +76,7 @@ def config_load():
         if t_dlfolder:
             dl_folder = t_dlfolder
 
+
 def config_save():
     cfg = get_config()
 
@@ -87,14 +87,15 @@ def config_save():
     if not config.has_section('path'):
         config.add_section('path')
 
-    config.set('path','path',basePath)
-    config.set('path','rootfolder',arcRoot)
-    config.set('path','dbfolder',db_folder)
-    config.set('path','dbname',db_name)
-    config.set('path','downloadfolder',dl_folder)
+    config.set('path', 'path', basePath)
+    config.set('path', 'rootfolder', arcRoot)
+    config.set('path', 'dbfolder', db_folder)
+    config.set('path', 'dbname', db_name)
+    config.set('path', 'downloadfolder', dl_folder)
 
     with open(cfg, 'w') as f:
         config.write(f)
+
 
 def IsNotStale(update_date, last_checked):
     wait = 0
@@ -137,6 +138,7 @@ def IsNotStale(update_date, last_checked):
 
     return ret
 
+
 def check_url(url):
     ret = False
 
@@ -149,16 +151,18 @@ def check_url(url):
 
     return ret
 
-def set_ffargs(location,folder):
 
-    parser = argparse.ArgumentParser(
-    description=('fanfic_scraper args'))
+def set_ffargs(location, folder):
+
+    parser = argparse.ArgumentParser(description=('fanfic_scraper args'))
 
     parser.add_argument(
-        "-f", "--folder", required=True, help="Name of folder to place fanfic in.")
+        "-f", "--folder", required=True,
+        help="Name of folder to place fanfic in.")
     parser.add_argument(
-        "-l", "--location", default=os.getcwd(), help="set download location")
-    #parser.add_argument(
+        "-l", "--location", default=os.getcwd(),
+        help="set download location")
+    # parser.add_argument(
     #    "-c", "--chapters", default=False,
     #    help="Specify chapters to download separated by : (10:15).")
     parser.add_argument(
@@ -171,82 +175,87 @@ def set_ffargs(location,folder):
         "-rt", "--retries", default=10,
         help="Number of retries before giving up")
 
-    args = parser.parse_args(['--location='+location,'--folder='+folder])
+    args = parser.parse_args(['--location=' + location, '--folder=' + folder])
 
     return args
+
 
 def print_list(matches):
     divisor = 6
     result = ''
 
-    for i in range(int(len(matches)/divisor)):
-            j = divisor * (i *1)
+    for i in range(int(len(matches) / divisor)):
+            j = divisor * (i * 1)
             result = ''
-            for j in range(j,j+(divisor)):
-                result=result+matches[j]+'\t'
+            for j in range(j, j + (divisor)):
+                result = result + matches[j] + '\t'
             print(result)
 
     k = len(matches) % divisor
-    l = (int(len(matches) / divisor)*divisor)
+    l = (int(len(matches) / divisor) * divisor)
 
     result = ''
-    for m in range(l,len(matches)):
-        result=result+matches[m]+'\t'
+    for m in range(l, len(matches)):
+        result = result + matches[m] + '\t'
     print(result)
+
 
 def get_subfolders(path):
 
-    subfolders = [f.name for f in os.scandir(path) if f.is_dir() ]
+    subfolders = [f.name for f in os.scandir(path) if f.is_dir()]
     subfolders.sort()
 
     return subfolders
 
-def get_files(path,value):
+
+def get_files(path, value):
     matches = []
     for root, dirs, files in os.walk(path):
-        for filename in fnmatch.filter(files,value):
+        for filename in fnmatch.filter(files, value):
             matches.append(filename)
     # Sort files alphabetically
     matches.sort()
 
     return matches
 
-def convert_file(source,htm):
 
-    txt = htm.split('.')[0]+'.txt'
-    ifile = [os.path.abspath(os.path.join(source,htm))]
-    ofile = os.path.abspath(os.path.join(source,txt))
-    cmd = ['html2text','--ignore-emphasis','-b 80']
+def convert_file(source, htm):
+
+    txt = htm.split('.')[0] + '.txt'
+    ifile = [os.path.abspath(os.path.join(source, htm))]
+    ofile = os.path.abspath(os.path.join(source, txt))
+    cmd = ['html2text', '--ignore-emphasis', '-b 80']
     msg = 'Converting file: {0} to text file: {1}'
 
-    print(msg.format(htm,txt))
+    print(msg.format(htm, txt))
 
     with open(ofile, "w") as outfile:
-        subprocess.call(cmd+ifile , stdout=outfile)
+        subprocess.call(cmd + ifile, stdout=outfile)
 
     for i in ifile:
         os.remove(i)
 
+
 class FanficDB:
 
     def __init__(self):
-        db_path = os.path.join(basePath,arcRoot,db_folder,db_name)
+        db_path = os.path.join(basePath, arcRoot, db_folder, db_name)
         dbhandle = DataBaseLogic(db_path)
 
     def add_story(self, folder, cat_id, url, info):
 
         with db_session:
-            Fanfic(Title = info['Title'],
-                   Author = info['Author'],
-                   Folder = folder,
-                   Count = 0,
-                   Description = info['Description'],
-                   Internet = url,
-                   StoryId = info['StoryId'],
-                   Publish_Date = info['Publish_Date'],
-                   Update_Date = info['Update_Date'],
-                   Last_Checked = info['Publish_Date'],
-                   Category_Id = cat_id)
+            Fanfic(Title=info['Title'],
+                   Author=info['Author'],
+                   Folder=folder,
+                   Count=0,
+                   Description=info['Description'],
+                   Internet=url,
+                   StoryId=info['StoryId'],
+                   Publish_Date=info['Publish_Date'],
+                   Update_Date=info['Update_Date'],
+                   Last_Checked=info['Publish_Date'],
+                   Category_Id=cat_id)
 
     def get_categories(self):
         d = defaultdict(list)
@@ -255,7 +264,7 @@ class FanficDB:
             categories = select(c for c in Category).order_by(Category.Name)
 
             for c in categories:
-                for k,v in c.to_dict().items():
+                for k, v in c.to_dict().items():
                     d[k].append(v)
 
         return d['Name']
@@ -269,7 +278,6 @@ class FanficDB:
 
         return folders
 
-
     def get_cat_id(self, categoryName):
 
         with db_session:
@@ -278,7 +286,7 @@ class FanficDB:
 
         return ret
 
-    def get_fanfics(self,cat_id):
+    def get_fanfics(self, cat_id):
         d = defaultdict(list)
         ret = []
 
@@ -291,7 +299,7 @@ class FanficDB:
 
             for f in ff:
                 d = defaultdict(list)
-                for k,v in f.to_dict().items():
+                for k, v in f.to_dict().items():
                     d[k].append(v)
                 ret.append(d)
 
@@ -315,22 +323,26 @@ class FanficDB:
             ff = Fanfic.get(Id=fic_id)
             ff.Update_Date = update
 
+
 def download_stories(args):
 
     cat = db.get_categories()
 
     for cat_name in cat:
-        #print("Processing:",cat_name)
+        # print("Processing:", cat_name)
         download_by_category(cat_name)
         convert_by_category(cat_name)
+
 
 def download_by_category(category):
 
     potential_keys = []
 
-    cat_folder = category.replace(' ','_')
+    cat_folder = category.replace(' ', '_')
 
-    location = os.path.join(basePath,arcRoot,dl_folder,cat_folder)
+    location = os.path.join(basePath, arcRoot, dl_folder, cat_folder)
+
+    ensure_dir(location)
 
     cat_id = db.get_cat_id(category)
 
@@ -342,7 +354,7 @@ def download_by_category(category):
 
     for fic in fics:
         fic_idx = fic_idx + 1
-        print(msg.format(category,fic_idx,fic_count),end='\r')
+        print(msg.format(category, fic_idx, fic_count), end='\r')
         title = fic['Title'][0]
         author = fic['Author'][0]
         folder = fic['Folder'][0]
@@ -361,22 +373,22 @@ def download_by_category(category):
         storyid = fic['StoryId'][0]
         fic_id = fic['Id'][0]
 
-        date_check = IsNotStale(update,last)
+        date_check = IsNotStale(update, last)
         url_check = check_url(url)
 
-        if url_check == True and date_check == True:
+        if url_check is True and date_check is True:
 
-            ffargs = set_ffargs(location,folder)
+            ffargs = set_ffargs(location, folder)
 
             next_chapter = chapters + 1
 
-            #Initialize class so we can retrieve actual story url
+            # Initialize class so we can retrieve actual story url
             fanfic = current_fanfic.fanfic(url, ffargs)
-            #Get fanfic url
+            # Get fanfic url
             url = fanfic.get_story_url(storyid)
-            #Initialize class with correct url.
+            # Initialize class with correct url.
             fanfic = current_fanfic.fanfic(url, ffargs)
-            #Download list of chapters
+            # Download list of chapters
             fanfic.get_chapters()
             # Get current chapter count from site
             cc = fanfic.chapter_count
@@ -386,49 +398,50 @@ def download_by_category(category):
                     potential_keys = [next_chapter]
                 else:
                     potential_keys = [
-                        i * 1 for i in range(next_chapter,(cc + 1))
-                        ]
-                #Show story about to be updated.
+                        i * 1 for i in range(next_chapter, (cc + 1))]
+                # Show story about to be updated.
                 update = fanfic.get_update_date()
-                print('',end='\n')
+                print('', end='\n')
                 print(title, '/', folder, '/', update)
-                #Set chapters to download
+                # Set chapters to download
                 fanfic.set_download_chapters(potential_keys)
-                #Download Story
+                # Download Story
                 fanfic.download_fanfic()
-                print('Downloaded fanfic: ',category,'/',title,'/',folder)
-                db.update_chapter_count(fic_id,cc)
-                db.update_last_updated(fic_id,update)
-                db.update_last_checked(fic_id,date.today())
+                print('Downloaded fanfic: ', category, '/', title, '/', folder)
+                db.update_chapter_count(fic_id, cc)
+                db.update_last_updated(fic_id, update)
+                db.update_last_checked(fic_id, date.today())
             else:
-                db.update_last_checked(fic_id,date.today())
+                db.update_last_checked(fic_id, date.today())
 
             sleep(4)
         else:
             sleep(0.2)
 
-    print('',end='\n')
+    print('', end='\n')
+
 
 def convert_by_category(category):
 
-    cat_folder = category.replace(' ','_')
-    location = os.path.join(basePath,arcRoot,dl_folder,cat_folder)
+    cat_folder = category.replace(' ', '_')
+    location = os.path.join(basePath, arcRoot, dl_folder, cat_folder)
 
     folders = get_subfolders(location)
 
     for folder in folders:
-        target = os.path.realpath(os.path.join(location,folder))
-        files = get_files(target,"*.htm")
+        target = os.path.realpath(os.path.join(location, folder))
+        files = get_files(target, "*.htm")
 
         for htm in files:
-            convert_file(target,htm)
+            convert_file(target, htm)
+
 
 def add_story(category, cat_id, folder):
-    _=os.system("clear")
+    _ = os.system("clear")
     url = input("Enter Story URL: ")
     url_check = check_url(url)
     if url_check:
-        ffargs = set_ffargs('/tmp',folder)
+        ffargs = set_ffargs('/tmp', folder)
         fanfic = current_fanfic.fanfic(url, ffargs)
         info = fanfic.story_info()
         print("ID: {0}".format(info['StoryId']))
@@ -450,6 +463,7 @@ def add_story(category, cat_id, folder):
         print("Site not supported!!!")
         cui.pause()
 
+
 def config_menu():
     category = "None"
     folder = "None"
@@ -461,7 +475,7 @@ def config_menu():
         menu.append("Add Story")
         menu.append("Exit")
 
-        _=os.system("clear")
+        _ = os.system("clear")
 
         print("Fanfic Downloader - Config")
         print("")
@@ -469,8 +483,8 @@ def config_menu():
 
         if "Category:" in ret:
             cat = db.get_categories()
-            _=os.system("clear")
-            category = cui.submenu(cat,"Choose Category: ")
+            _ = os.system("clear")
+            category = cui.submenu(cat, "Choose Category: ")
             cat_id = db.get_cat_id(category)
         if "Folder:" in ret:
             if category != "None":
@@ -501,7 +515,8 @@ def main():
             'Automates fanfic downloads latest chapters using fanfic_scraper. '
             '(Currently works with fanfiction.net)'))
     parser.add_argument(
-        "-c", '--config', required=False, action='store_true', help="Configures Fanfic Downloader.")
+        "-c", '--config', required=False, action='store_true',
+        help="Configures Fanfic Downloader.")
 
     config_load()
     db = FanficDB()
