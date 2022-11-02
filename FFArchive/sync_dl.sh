c7a8ae887fad858dba99a7ea92140e1177a023c9
Kiera Meredith <kmeredith@teamscci.com>
2022-11-02 10:31:23 -0400

    Restructure Solution

--- FFArchive/sync_dl.sh
+++
@@ -1,2 +0,0 @@
-rclone sync -v onedrive:FF_Archive /opt/home/ubuntu/OneDrive/FF_Archive
-rclone sync -v onedrive:Fanfiction /opt/home/ubuntu/OneDrive/Fanfiction
--- FFArchive/sync_ul.sh
+++
@@ -1,4 +0,0 @@
-rclone sync -v /opt/home/ubuntu/OneDrive/FF_Archive onedrive:FF_Archive 
-rclone sync -v /opt/home/ubuntu/OneDrive/FF_Archive/Current_FF onedrive:Fanfiction
-#rclone sync -v /opt/home/ubuntu/OneDrive/Fiction onedrive:Fiction
- 
